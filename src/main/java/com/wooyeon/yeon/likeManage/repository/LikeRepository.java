167496e5bd1c2837280918b1f184a9bb7901cc28
hesoo98 <hesoo98@naver.com>
2023-07-09 23:35:18 +0900

    [ETC] 연관관계 수정, 클래스 이름 변경(mysql겹침,Like->UserLike)

--- src/main/java/com/wooyeon/yeon/likeManage/repository/LikeRepository.java
+++ src/main/java/com/wooyeon/yeon/likeManage/repository/LikeRepository.java
@@ -1,7 +1,7 @@
 package com.wooyeon.yeon.likeManage.repository;
 
-import com.wooyeon.yeon.likeManage.domain.Like;
+import com.wooyeon.yeon.likeManage.domain.UserLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LikeRepository extends JpaRepository<Like, Long> {
+public interface LikeRepository extends JpaRepository<UserLike, Long> {
 }
