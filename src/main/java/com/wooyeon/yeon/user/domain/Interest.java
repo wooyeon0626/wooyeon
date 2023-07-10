56b9e43cf4d5ed240d48db34181353d4b4b69bd5
ez0 <101056943+easyoungcode@users.noreply.github.com>
2023-07-10 19:08:31 +0900

    [ETC] USER 쪽 List로 선언된 ProfilePhotos, Hobbys, Interests와 각자 엔티티의 연관관계(1:n) 설정 및 수정

--- src/main/java/com/wooyeon/yeon/user/domain/Interest.java
+++ src/main/java/com/wooyeon/yeon/user/domain/Interest.java
@@ -1,11 +1,13 @@
 package com.wooyeon.yeon.user.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Interest {
     @Id
     private Long interestId;
     private String interest;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private User user;
 }
