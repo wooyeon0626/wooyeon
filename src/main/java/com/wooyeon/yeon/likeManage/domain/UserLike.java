20c4c0f91cb41760ecb8e23715a2364ce6515991
hesoo98 <hesoo98@naver.com>
2023-07-10 21:03:40 +0900

    [REFACTOR] UserLike안의 UserMatch컬럼을 삭제, OnetoOne을 ManyToOne으로 수정, UserMatch안에 UserLike의 ID를 OneToOne 단방향으로 맵핑(외래키) 두개 입력받게 추가

--- src/main/java/com/wooyeon/yeon/likeManage/domain/UserLike.java
+++ src/main/java/com/wooyeon/yeon/likeManage/domain/UserLike.java
@@ -1,6 +1,5 @@
 package com.wooyeon.yeon.likeManage.domain;
 
-import com.wooyeon.yeon.profileChoice.domain.UserMatch;
 import com.wooyeon.yeon.user.domain.User;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -16,22 +15,17 @@ public class UserLike {
     private Long likeId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "like_to", nullable = false)
+    @JoinColumn(name = "like_to_user_id", nullable = false)
     private User likeTo; // 누구를 좋아하니?
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "like_from", nullable = false)
+    @JoinColumn(name = "like_from_user_id", nullable = false)
     private User likeFrom; // 누가
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "match_id")
-    private UserMatch userMatch;
-
     @Builder
-    public UserLike(Long likeId, User likeTo, User likeFrom, UserMatch userMatch) {
+    public UserLike(Long likeId, User likeTo, User likeFrom) {
         this.likeId = likeId;
         this.likeTo = likeTo;
         this.likeFrom = likeFrom;
-        this.userMatch = userMatch;
     }
 }
