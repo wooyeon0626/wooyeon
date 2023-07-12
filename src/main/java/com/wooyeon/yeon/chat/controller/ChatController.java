378aa28f23b91ef35a12be27490aff69d311d98c
tenta <eunoia3802@gmail.com>
2023-07-12 20:12:40 +0900

    [FEAT] 사용자로부터 입력된 Message 정보 DB 저장 기능 구현

--- src/main/java/com/wooyeon/yeon/chat/controller/ChatController.java
+++ src/main/java/com/wooyeon/yeon/chat/controller/ChatController.java
@@ -16,6 +16,10 @@ public class ChatController {
         /app/chat                     - 메시지 발생
     */
 
+    /**
+     * @param chatDto
+     * @return 매치 아이디에 따른 구독 채팅방으로 chatDto 값 전송
+     */
     @MessageMapping("/chat")
     public void enter(ChatDto chatDto) {
         if(ChatDto.MessageType.ENTER.equals(chatDto.getType())) {
