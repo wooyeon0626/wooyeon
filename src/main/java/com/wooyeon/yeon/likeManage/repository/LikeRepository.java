07d30926fbe2e26b55f9c844a8c82a0aa487f839
hesoo98 <hesoo98@naver.com>
2023-07-15 17:23:45 +0900

    LikeService에서 like 생성, match 체크 하는 메서드 구현, MatchService에서 Match생성하는 메서드 구현

--- src/main/java/com/wooyeon/yeon/likeManage/repository/LikeRepository.java
+++ src/main/java/com/wooyeon/yeon/likeManage/repository/LikeRepository.java
@@ -1,7 +1,9 @@
 package com.wooyeon.yeon.likeManage.repository;
 
 import com.wooyeon.yeon.likeManage.domain.UserLike;
+import com.wooyeon.yeon.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikeRepository extends JpaRepository<UserLike, Long> {
+    UserLike findByLikeFromAndLikeTo(User likeFrom, User likeTo);//LikeFrom과 LikeTo를 찾는다.
 }
--- src/main/java/com/wooyeon/yeon/likeManage/service/LikeService.java
+++ src/main/java/com/wooyeon/yeon/likeManage/service/LikeService.java
@@ -1,6 +1,8 @@
 package com.wooyeon.yeon.likeManage.service;
 
+import com.wooyeon.yeon.likeManage.domain.UserLike;
 import com.wooyeon.yeon.likeManage.repository.LikeRepository;
+import com.wooyeon.yeon.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +10,23 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LikeService {
     private final LikeRepository likeRepository;
+
+    //like 생성
+    public UserLike createLike(User likeFrom, User likeTo) {
+        UserLike userLike = UserLike.builder()
+                .likeFrom(likeFrom)
+                .likeTo(likeTo)
+                .build();
+
+        return likeRepository.save(userLike);
+    }
+
+    //매치되었는지 확인 하는 메서드
+    public boolean checkMatch(User user1, User user2) {
+        UserLike like1 = likeRepository.findByLikeFromAndLikeTo(user1, user2);
+        UserLike like2 = likeRepository.findByLikeFromAndLikeTo(user2, user1);
+
+        return like1 != null && like2 != null;
+    }
+
 }
