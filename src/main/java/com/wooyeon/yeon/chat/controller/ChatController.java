9ac7b5cfe81d354fa1c58818031091e7fc40d4c8
tenta <eunoia3802@gmail.com>
2023-07-15 15:24:45 +0900

    [REFACTOR] WebSocket Endpoint path 변경 및 MessageMapping 경로 변경

--- src/main/java/com/wooyeon/yeon/chat/controller/ChatController.java
+++ src/main/java/com/wooyeon/yeon/chat/controller/ChatController.java
@@ -13,18 +13,14 @@ public class ChatController {
 
     /*
         /queue/chat/room/{matchId}    - 구독
-        /app/chat                     - 메시지 발생
+        /app/chat/message             - 메시지 발생
     */
 
-    /**
-     * @param chatDto
-     * @return 매치 아이디에 따른 구독 채팅방으로 chatDto 값 전송
-     */
-    @MessageMapping("/chat")
-    public void enter(ChatDto chatDto) {
-        if(ChatDto.MessageType.ENTER.equals(chatDto.getType())) {
-            chatDto.setMessage(chatDto.getSender() + "이 입장했습니다.");
+    @MessageMapping("/chat/message")
+    public void enter(ChatDto message) {
+        if (ChatDto.MessageType.ENTER.equals(message.getType())) {
+            message.setMessage(message.getSender()+"님이 입장하였습니다.");
         }
-        simpMessageSendingOperations.convertAndSend("/queue/chat/room/" + chatDto.getMatchId(), chatDto);
+        simpMessageSendingOperations.convertAndSend("/queue/chat/room/"+message.getRoomId(),message);
     }
 }
