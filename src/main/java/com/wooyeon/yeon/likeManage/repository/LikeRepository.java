1c2f3960b95b0b4d2fe89b91fed05e9b25a424de
hesoo98 <hesoo98@naver.com>
2023-07-30 18:48:36 +0900

    [ETC] 오류 안뜨게 잠시 수정

--- src/main/java/com/wooyeon/yeon/likeManage/repository/LikeRepository.java
+++ src/main/java/com/wooyeon/yeon/likeManage/repository/LikeRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface LikeRepository extends JpaRepository<UserLike, Long> {
     UserLike findByLikeFromAndLikeTo(User likeFrom, User likeTo);//LikeFrom과 LikeTo를 찾는다.
 
-    UserLike findLikeForMe()
+    //UserLike findLikeForMe()
 }
