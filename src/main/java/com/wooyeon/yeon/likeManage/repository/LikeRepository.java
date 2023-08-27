c31ad7f133cb4ff64ec453529cc312e8ec5c24f0
hesoo98 <hesoo98@naver.com>
2023-08-27 17:49:43 +0900

    [FEAT] 좋아요 이후 매치되면 매치 테이블 추가

--- src/main/java/com/wooyeon/yeon/likeManage/repository/LikeRepository.java
+++ src/main/java/com/wooyeon/yeon/likeManage/repository/LikeRepository.java
@@ -11,4 +11,7 @@ public interface LikeRepository extends JpaRepository<UserLike, Long> {
     @Query("select count(*) from UserLike userLike " +
             "where (userLike.likeFrom.userId = :fromId and userLike.likeTo.userId = :toId) ")
     long countMatch(@Param("fromId") Long fromId, @Param("toId") Long toId);
+
+
+    UserLike findByLikeFromAndLikeTo(User likeFromUser, User likeToUser);
 }
--- src/main/java/com/wooyeon/yeon/profileChoice/service/MatchService.java
+++ src/main/java/com/wooyeon/yeon/profileChoice/service/MatchService.java
@@ -1,12 +1,30 @@
 package com.wooyeon.yeon.profileChoice.service;
 
+import com.wooyeon.yeon.likeManage.domain.UserLike;
+import com.wooyeon.yeon.profileChoice.domain.UserMatch;
 import com.wooyeon.yeon.profileChoice.repository.MatchRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Timestamp;
 
 
 @Service
 @RequiredArgsConstructor
 public class MatchService {
     private final MatchRepository matchRepository;
+
+    @Transactional
+    public String createMatch(UserLike userLike1, UserLike userLike2) {
+        Timestamp currentTimestamp = new Timestamp(System.currentTimeMillis());
+        UserMatch userMatch = UserMatch.builder()
+                .userLike1(userLike1)
+                .userLike2(userLike2)
+                .generateTime(currentTimestamp)
+                .build();
+
+        matchRepository.save(userMatch);
+        return "매치테이블 생성";
+    }
 }
