d615b670d858942eba2127d58af4d175faaeba12
ez0 <101056943+easyoungcode@users.noreply.github.com>
2024-01-28 16:43:01 +0900

    [FEAT] 암호화된 비밀번호 RSA 개인키로 복호화, salt 생성 후 비밀번호+salt를 SHA256으로 암호화 후 User에 저장 1. RSA 개인키로 복호화 2. salt 생성 구현 3. SHA256 암호화 구현 4. 비밀번호+salt를 SHA256으로 암호화 후 USER에 저장 5. USER 엔티티에 salt 컬럼 추가

--- src/main/java/com/wooyeon/yeon/user/service/UserService.java
+++ src/main/java/com/wooyeon/yeon/user/service/UserService.java
@@ -1,15 +1,25 @@
 package com.wooyeon.yeon.user.service;
 
 import com.wooyeon.yeon.user.domain.User;
+import com.wooyeon.yeon.user.dto.PasswordEncryptRequestDto;
+import com.wooyeon.yeon.user.dto.PasswordEncryptResponseDto;
 import com.wooyeon.yeon.user.dto.RsaPublicResponseDto;
-import com.wooyeon.yeon.user.repository.EmailAuthRepository;
 import com.wooyeon.yeon.user.repository.UserRepository;
 import com.wooyeon.yeon.user.service.encrypt.RsaUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
 import java.util.UUID;
 
 @Slf4j
@@ -32,11 +42,95 @@ public class UserService {
 
     public RsaPublicResponseDto sendRsaPublicKey() {
         RsaPublicResponseDto rsaPublicResponseDto = RsaPublicResponseDto.builder()
-                .publicKey(rsaUtil.sendPublicKey())
+                .publicKey(RsaUtil.sendPublicKey())
                 .build();
 
-        log.info("Service public key: {}", rsaUtil.sendPublicKey());
+        log.info("Service public key: {}", RsaUtil.sendPublicKey());
 
         return rsaPublicResponseDto;
     }
+
+    public PasswordEncryptResponseDto decodeEncrypt(PasswordEncryptRequestDto passwordEncryptRequestDto)
+            throws NoSuchPaddingException, IllegalBlockSizeException, InvalidKeySpecException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+
+        // AES 암호화 키 및 IV 받아오기
+
+
+        String encryptedPassword = "ABCDEFGHI1234567";
+        log.info("RSA 공개키로 암호화 된 암호(encryptedPassword) : {}",encryptedPassword);
+
+        // RSA 개인키로 복호화해서 비밀번호 원문 받아오기
+        String decodedPassword = RsaUtil.rsaDecode(encryptedPassword, RsaUtil.sendPrivateKey());
+        log.info("password 원문 : {}", decodedPassword);
+
+        // 비밀번호 + salt를 SHA256으로 암호화
+        String salt = createSalt();
+        String password = decodedPassword+salt;
+        String finalPassword = encryptSha256(password);
+
+        // User 테이블에 저장
+        User user = User.builder()
+                .email(passwordEncryptRequestDto.getEmail())
+                .emailAuth(true)
+                .userCode(UUID.randomUUID())
+                .password(finalPassword)
+                .salt(salt)
+                .build();
+        userRepository.save(user);
+
+        // ResponseDto 구성
+        PasswordEncryptResponseDto passwordEncryptResponseDto = PasswordEncryptResponseDto.builder()
+                .statusCode(HttpStatus.SC_OK)
+                .statusName("success")
+                .build();
+        return passwordEncryptResponseDto;
+    }
+
+    // salt 생성
+    public String createSalt() {
+
+        //1. Random, byte 객체 생성
+        SecureRandom r = new SecureRandom ();
+        byte[] salt = new byte[20];
+
+        //2. 난수 생성
+        r.nextBytes(salt);
+
+        //3. byte To String (10진수의 문자열로 변경)
+        StringBuffer sb = new StringBuffer();
+        for(byte b : salt) {
+            sb.append(String.format("%02x", b));
+        };
+
+        return sb.toString();
+    }
+
+    // SHA256 암호화
+    public String encryptSha256(String password) {
+
+        String shaEncryptedPassword = "";
+        try {
+            //1. SHA256 알고리즘 객체 생성
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+            //2. 비밀번호와 salt 합친 문자열에 SHA 256 적용
+            md.update(password.getBytes());
+            byte[] pwdSalt = md.digest();
+
+            //3. byte To String (10진수의 문자열로 변경)
+            StringBuffer sb = new StringBuffer();
+            for (byte b : pwdSalt) {
+                sb.append(String.format("%02x", b));
+            }
+
+            shaEncryptedPassword=sb.toString();
+            log.info("SHA 암호화 후 : {}", shaEncryptedPassword);
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return shaEncryptedPassword;
+    }
+
 }
