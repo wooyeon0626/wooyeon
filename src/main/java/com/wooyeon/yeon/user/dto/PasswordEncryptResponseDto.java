7a6fb56aece343f86481d230bf8eba5c57e28661
easyoungcode <gchun910@gmail.com>
2024-01-30 23:00:04 +0900

    [FIX] DefaultHandlerExceptionResolver : Resolved 406에러 해결

--- src/main/java/com/wooyeon/yeon/user/dto/PasswordEncryptResponseDto.java
+++ src/main/java/com/wooyeon/yeon/user/dto/PasswordEncryptResponseDto.java
@@ -2,10 +2,12 @@ package com.wooyeon.yeon.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Builder
+@Getter
 public class PasswordEncryptResponseDto {
     private int statusCode;
     private String statusName;
